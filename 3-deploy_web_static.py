8230fb34ec4d7cb8f6299dabc6de75ee83737e41
luismedinaeng <malf95@hotmail.com>
2020-04-22 13:09:10 -0500

    [Add]Complete Task 3

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+'''
+Module that generates tgz file with the content of web_server directories
+'''
+import datetime
+from fabric.api import *
+
+env.hosts = ['34.74.55.34', '3.82.65.220']
+
+
+def do_pack():
+    local("mkdir -p versions")
+    current = datetime.datetime.now()
+    file_tgz = "versions/web_static_{}.tgz".format(current.strftime("%Y%m%d%H%M%S"))
+    if local("tar -cvzf {} web_static".format(file_tgz)).failed:
+        return None
+    else:
+        return file_tgz
+
+
+def do_deploy(archive_path):
+    if local("ls {}".format(archive_path)).failed:
+        return False
+    archive_name = archive_path.split('/')[-1]
+    folder = archive_name.split('.')[0]
+    if put(archive_path, "/tmp/{}".format(archive_name)).failed:
+        return False
+    run("mkdir -p /data/web_static/releases/{}".format(folder))
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".
+           format(archive_name, folder)).failed:
+        return False
+    run("rm /tmp/{}".format(archive_name))
+    run("mv {0}/web_static/* {0}/"
+        .format("/data/web_static/releases/" + folder))
+    run("rm -rf /data/web_static/realeases/{}/web_static".format(folder))
+    run("rm -rf /data/web_static/current")
+    run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+        .format(folder))
+    return True
+
+def deploy():
+    archive_path = do_pack()
+    if archive_path == None:
+        return False
+    else:
+        return do_deploy(archive_path)
