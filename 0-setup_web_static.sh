72698552ab20279a007d2423337f5e6a8b747d2b
luismedinaeng <malf95@hotmail.com>
2020-04-21 22:52:15 -0500

    [Fix]Add the hbnb_static dir to the server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 # Script that install nginx and setup the server to
 # serve the content of /data/web_static/current to hbnb_static
+apt-get update --assume-yes
 apt-get install nginx --assume-yes
 mkdir --parents /data/web_static/releases/test
 mkdir --parents /data/web_static/shared
-echo "Hello World! This is a test file" >> /data/web_static/releases/test/index.html
+echo "Hello World! This is a test file" > /data/web_static/releases/test/index.html
 ln --force --symbolic /data/web_static/releases/test /data/web_static/current
 chown --recursive ubuntu:ubuntu /data
-OLD="server_name _;"
-NEW="server_name _;\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t}"
-sed -i "s/$OLD/$NEW/g" /etc/nginx/sites-available/default
+OLD=$(cat /etc/nginx/sites-available/default | grep '^\s*server_name .*;$' --max-count=1)
+NEW="\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t}"
+sed -i "0,/$OLD/s//$NEW/" /etc/nginx/sites-available/default
+rm /etc/nginx/sites-enabled/default
+ln --symbolic /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 service nginx restart
