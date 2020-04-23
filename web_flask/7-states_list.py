54a1680b679cf1ae9b017670ca41971323c57c7e
luismedinaeng <malf95@hotmail.com>
2020-04-22 20:00:49 -0500

    [Add]Complete Task 8 - Listing states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+from models import storage, State
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def close_session():
+    storage.close()
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    states = storage.all(State)
+    return render_tenplate("7-states_list.html", states=states)
+
+if __name__ = "__main__":
+    app.run(host='0.0.0.0', port=5000)
