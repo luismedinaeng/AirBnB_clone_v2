5bbc22a74f49a9de437f1110a04bd61f6656ba7c
luismedinaeng <malf95@hotmail.com>
2020-04-22 22:54:58 -0500

    [Add]Complete Task 10 - Lists states with or without an id

---
+++ web_flask/9-states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Flask web app that lists states
+"""
+from flask import Flask, render_template
+from models import storage, State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_session(r):
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def cities_by_states(state_id=None):
+    states = storage.all(State)
+    if state_id is None:
+        states = states.values()
+    else:
+        key = "State.{}".format(state_id)
+        if key in states:
+            states = [states[key]]
+        else:
+            states = []
+    return render_template("9-states.html", states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
