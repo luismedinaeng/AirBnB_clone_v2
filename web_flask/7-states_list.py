d185684007c3d2f0d43077526e7316a2dc10a3cf
luismedinaeng <malf95@hotmail.com>
2020-04-22 20:13:17 -0500

    [Update]Listing states sorted by name

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,14 +4,18 @@ from models import storage, State
 
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def close_session(r):
     storage.close()
 
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     states = storage.all(State)
+    states = states.values()
     return render_template("7-states_list.html", states=states)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
