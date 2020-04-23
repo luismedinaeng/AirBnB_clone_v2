86b97f2da3d627d81d254934b6f68f28e0a2635d
luismedinaeng <malf95@hotmail.com>
2020-04-22 20:09:03 -0500

    [Update]Last changes for states_list route

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,13 +5,13 @@ from models import storage, State
 app = Flask(__name__)
 
 @app.teardown_appcontext
-def close_session():
+def close_session(r):
     storage.close()
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     states = storage.all(State)
-    return render_tenplate("7-states_list.html", states=states)
+    return render_template("7-states_list.html", states=states)
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
